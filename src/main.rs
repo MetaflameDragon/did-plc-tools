2b3c0cec0d973ac6d46d9e56342edc51af97d553
Metaflame <metaflamedragon@gmail.com>
2025-01-05 04:42:31 +0100

    Tweaks, add serde, add field docs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use did_key::{
     self, Config, DIDCore, Fingerprint, KeyMaterial, P256KeyPair, PatchedKeyPair, Secp256k1KeyPair,
     CONFIG_LD_PUBLIC,
 };
-use serde::Deserialize;
-use std::{collections::HashMap, fmt::Pointer};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, fmt::Pointer, iter};
 use url::Url;
 
 fn main() {
@@ -12,7 +12,9 @@ fn main() {
 
     let seed = None;
     let signing_key = did_key::generate::<Secp256k1KeyPair>(seed);
-    let rotation_keys = vec![did_key::generate::<Secp256k1KeyPair>(seed)];
+    let rotation_keys = iter::from_fn(|| Some(did_key::generate::<Secp256k1KeyPair>(seed)))
+        .take(2)
+        .collect();
 
     let unsigned_op = UnsignedPlcOperation {
         r#type: "plc_operation".to_string(),
@@ -26,7 +28,7 @@ fn main() {
             "atproto_pds".to_string(),
             PlcService {
                 r#type: "AtprotoPersonalDataServer".to_string(),
-                endpoint,
+                endpoint: endpoint.to_string(),
             },
         )]),
         prev: None,
@@ -38,33 +40,44 @@ fn main() {
 fn format_did_key(did_key: &PatchedKeyPair) -> DidKey {
     DidKey(format!("did:key:{}", did_key.fingerprint()))
 }
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct DidKey(String);
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Signature(String);
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct AkaUri(String);
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct PlcService {
     r#type: String,
-    endpoint: Url,
+    endpoint: String,
 }
 
-#[derive(Debug)]
-struct PlcOperation<'a> {
-    inner: &'a UnsignedPlcOperation<'a>,
+#[derive(Debug, Serialize, Deserialize)]
+struct PlcOperationRef(String);
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PlcOperation {
+    inner: UnsignedPlcOperation,
     sig: Signature,
 }
 
-#[derive(Debug)]
-struct UnsignedPlcOperation<'a> {
+#[derive(Debug, Serialize, Deserialize)]
+struct UnsignedPlcOperation {
+    // Fixed value "plc_operation"
     r#type: String,
+    // Array of up to 5 rotation keys
     rotation_keys: Vec<DidKey>,
+    // Key-value map of verification methods (e.g. "atproto" & signing key)
     verification_methods: HashMap<String, DidKey>,
+    // Array of at:// handles
     also_known_as: Vec<AkaUri>,
+    // Key-value map of services, services must have a type and endpoint.
+    // Endpoint must be a valid http(s)-prefixed url
+    // Key is currently just "atproto_pds" for type "AtprotoPersonalDataServer"
     services: HashMap<String, PlcService>,
-    prev: Option<&'a PlcOperation<'a>>,
+    // CID Hash reference to previous operation, null (None) for genesis operations
+    prev: Option<PlcOperationRef>,
 }

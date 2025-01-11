2e09049e0a6f97a7cdeec2120ce7312283bf1d63
Metaflame <metaflamedragon@gmail.com>
2025-01-11 04:35:03 +0100

    Rework to use traits

--- did-key/src/lib.rs
+++ did-key/src/lib.rs
@@ -1,5 +1,8 @@
 use derive_more::Into;
 use multibase::Base;
+use p256::PublicKey as P256PublicKey;
+use secp256k1::PublicKey as Secp256k1PublicKey;
+use serde::Serialize;
 use std::io::Read;
 
 /// A String newtype representing public key bytes in the did:key:<mb-value>
@@ -11,11 +14,10 @@ pub struct DidKey {
 }
 
 impl DidKey {
-    pub fn from_public_key(multicodec_key_bytes: &[u8], public_key_bytes: &[u8]) -> DidKey {
-        // TODO: Check key byte count
+    pub fn from_public_key<K: SupportedKeyAlgo>(public_key: K) -> DidKey {
         let mb_value = multibase::encode(
             Base::Base58Btc,
-            itertools::chain(public_key_bytes, multicodec_key_bytes)
+            itertools::chain(K::multicodec_bytes(), public_key.pub_key_bytes().iter())
                 .copied()
                 .collect::<Vec<u8>>(),
         );
@@ -30,6 +32,32 @@ impl DidKey {
     }
 }
 
+pub trait SupportedKeyAlgo: Multicodec {
+    fn pub_key_bytes(&self) -> Box<[u8]>;
+}
+
+pub trait Multicodec {
+    fn multicodec_bytes() -> &'static [u8];
+}
+
+impl Multicodec for P256PublicKey {
+    fn multicodec_bytes() -> &'static [u8] {
+        &[0x12, 0x00]
+    }
+}
+
+impl Multicodec for Secp256k1PublicKey {
+    fn multicodec_bytes() -> &'static [u8] {
+        &[0xe7]
+    }
+}
+
+impl SupportedKeyAlgo for Secp256k1PublicKey {
+    fn pub_key_bytes(&self) -> Box<[u8]> {
+        Box::new(self.serialize())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -40,7 +68,7 @@ mod tests {
         let mut rng = StepRng::new(2, 1);
         let (_secret_key, public_key) = secp256k1::generate_keypair(&mut rng);
 
-        let did_key = DidKey::from_public_key(&[0xe7], &public_key.serialize());
+        let did_key = DidKey::from_public_key(public_key);
 
         assert_eq!(
             did_key.formatted_value(),

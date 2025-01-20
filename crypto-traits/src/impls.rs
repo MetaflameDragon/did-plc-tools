ee0cce05a61272171b4d531413d04c2230208b4f
Metaflame <metaflamedragon@gmail.com>
2025-01-20 19:53:41 +0100

    Fix impl to specifically use public key types

--- crypto-traits/src/impls.rs
+++ crypto-traits/src/impls.rs
@@ -1,12 +1,12 @@
 use crate::traits::*;
 
-impl MulticodecPrefix for p256::NistP256 {
+impl MulticodecPrefix for p256::PublicKey {
     fn multicodec_prefix_raw() -> u64 {
         0x1200
     }
 }
 
-impl MulticodecPrefix for k256::Secp256k1 {
+impl MulticodecPrefix for k256::PublicKey {
     fn multicodec_prefix_raw() -> u64 {
         0xe7
     }
@@ -17,13 +17,13 @@ mod tests {
     use super::*;
     #[test]
     fn varint_p256() {
-        let varint = p256::NistP256::multicodec_prefix_unsigned_varint();
+        let varint = p256::PublicKey::multicodec_prefix_unsigned_varint();
         assert_eq!(varint, &[0x80, 0x24])
     }
 
     #[test]
     fn varint_k256() {
-        let varint = k256::Secp256k1::multicodec_prefix_unsigned_varint();
+        let varint = k256::PublicKey::multicodec_prefix_unsigned_varint();
         assert_eq!(varint, &[0xe7, 0x01])
     }
 }

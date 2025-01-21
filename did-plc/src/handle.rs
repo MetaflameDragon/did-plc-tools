603589bdd7a65ac04e198e315d82b9795cabe4b6
Metaflame <metaflamedragon@gmail.com>
2025-01-21 21:04:42 +0100

    Add handle validation

--- did-plc/src/handle.rs
+++ did-plc/src/handle.rs
@@ -1,13 +1,19 @@
 use thiserror::Error;
 
-#[derive(Error, Debug, Eq, PartialEq, Clone, Copy, Hash)]
+pub const MAX_HANDLE_LENGTH: usize = 253;
+pub const MAX_SEGMENT_LENGTH: usize = 63;
+
+#[derive(Error, Debug, Eq, PartialEq, Clone, Hash)]
 pub enum Error {
+    #[error("Handle can be at most 253 characters long")]
+    HandleTooLong,
     #[error("Handle cannot have leading or trailing periods")]
     LeadingOrTrailingPeriods,
     #[error("Handle must have at least two segments (at least one period)")]
     OnlyOneSegment,
-    #[error("Handle contains an invalid segment")] // Can add details later
-    InvalidSegment,
+    #[error(r#"Handle contains an invalid segment (see handle::validate_handle): "`{0}`""#)]
+    // Can add details later
+    InvalidSegment(String),
 }
 
 // TODO: validate against known disallowed TLDs?
@@ -15,15 +21,20 @@ pub enum Error {
 /// Validates a handle according to rules:
 /// - Only `[a-z]`, `[0-9]`, `-` are allowed segment characters, `.` is the separator
 ///  - Handles are not case-sensitive, `[A-Z]` is also valid
+/// - Maximum length of 253 characters
 /// - No leading or trailing `.`
 /// - Handle must have at least two segments
-/// - Segments cannot be empty
+/// - Segments must have a length between 1 and 63 chars
 /// - Segments cannot start or end with a hyphen
 /// - Last segment (TLD) cannot start with a numeric digit
 ///
 /// Follows specs as defined [here](https://atproto.com/specs/handle#handle-identifier-syntax)
 ///
 pub fn validate_handle(input: &str) -> Result<(), Error> {
+    if input.len() > MAX_HANDLE_LENGTH {
+        return Err(Error::HandleTooLong);
+    }
+
     if input.starts_with('.') || input.ends_with('.') {
         return Err(Error::LeadingOrTrailingPeriods);
     }
@@ -33,25 +44,149 @@ pub fn validate_handle(input: &str) -> Result<(), Error> {
         return Err(Error::OnlyOneSegment);
     }
 
-    // Leading TLD digit
+    // Leading TLD digit disallowed
     let re_num_start = regex::Regex::new(r#"^\d"#).unwrap();
-    if !re_num_start.is_match(segments.last().unwrap()) {
-        return Err(Error::InvalidSegment);
+    let last_segment = segments.last().unwrap();
+    if re_num_start.is_match(last_segment) {
+        return Err(Error::InvalidSegment(last_segment.to_string()));
     }
 
-
-    let re_segment = regex::Regex::new(r#"^[0-9a-z][0-9a-z\-]+"#).unwrap();
+    // Doesn't normalize! checks for a-z & A-Z instead
+    let re_segment = regex::Regex::new(r#"^[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?$"#).unwrap();
 
     // Segment validation
     for seg in segments {
-        // Cannot be empty
-        if seg.is_empty() {
-            return Err(Error::InvalidSegment);
+        // Segments must be at most 63 characters long
+        if seg.len() > MAX_SEGMENT_LENGTH {
+            return Err(Error::InvalidSegment(seg.to_string()));
         }
+        // Must match pattern
+        if !re_segment.is_match(seg) {
+            return Err(Error::InvalidSegment(seg.to_string()));
+        }
+    }
 
-        // Cannot start or end with '-'
-        if seg.starts_with('-') || seg.ends_with('.') {
-            return Err(Error::InvalidSegment);
-        }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_domain() {
+        let input = "metaflame.dev";
+        assert!(validate_handle(input).is_ok());
+    }
+
+    #[test]
+    fn max_length_handle() {
+        let input =
+            "lo-rem.ipsum.dolor.sit.amet.consectetur.adipiscing.elit.maecenas.eget.justo.sed.\
+            velit.pellentesque.consectetur.eu.vel.turpis.aenean.nulla.diam.elementum.non.nisl.\
+            eget.fermentum.aliquet.erat.sed.ultricies.turpis.quis.ante.egestas.lobortis.sed.\
+            turpis.quam";
+        assert_eq!(
+            input.len(),
+            MAX_HANDLE_LENGTH,
+            "Input handle length sanity check"
+        );
+        assert_eq!(validate_handle(input), Ok(()));
+    }
+
+    #[test]
+    fn too_long_handle() {
+        let input =
+            "lorem.ipsum.dolor.sit.amet.consectetur.adipiscing.elit.maecenas.eget.justo.sed.\
+            velit.pellentesque.consectetur.eu.vel.turpis.aenean.nulla.diam.elementum.non.nisl.\
+            eget.fermentum.aliquet.erat.sed.ultricies.turpis.quis.ante.egestas.lobortis.sed.\
+            turpis.quam.finibus";
+        assert!(
+            input.len() > MAX_HANDLE_LENGTH,
+            "Input handle length sanity check"
+        );
+        assert_eq!(validate_handle(input), Err(Error::HandleTooLong));
+    }
+
+    #[test]
+    fn reject_one_segment() {
+        let input = "com";
+        assert_eq!(validate_handle(input), Err(Error::OnlyOneSegment));
+    }
+
+    #[test]
+    fn reject_leading_tld_number() {
+        let input = "example.1xy";
+        assert_eq!(
+            validate_handle(input),
+            Err(Error::InvalidSegment("1xy".to_string()))
+        );
+    }
+
+    #[test]
+    fn reject_leading_hyphen() {
+        let input = "sub.-example.test";
+        assert_eq!(
+            validate_handle(input),
+            Err(Error::InvalidSegment("-example".to_string()))
+        );
+    }
+
+    #[test]
+    fn reject_trailing_hyphen() {
+        let input = "sub.example-.test";
+        assert_eq!(
+            validate_handle(input),
+            Err(Error::InvalidSegment("example-".to_string()))
+        );
+    }
+
+    #[test]
+    fn reject_empty_segment() {
+        let input = "sub..test";
+        assert_eq!(
+            validate_handle(input),
+            Err(Error::InvalidSegment("".to_string()))
+        );
+    }
+
+    #[test]
+    fn reject_leading_period() {
+        let input = ".example.test";
+        assert_eq!(validate_handle(input), Err(Error::LeadingOrTrailingPeriods));
+    }
+
+    #[test]
+    fn reject_trailing_period() {
+        let input = "example.test.";
+        assert_eq!(validate_handle(input), Err(Error::LeadingOrTrailingPeriods));
+    }
+
+    #[test]
+    fn accept_max_length_segment() {
+        let long_segment = "loremipsumdolorsitametconsecteturadipiscingelitmaecenasegetjust";
+        assert_eq!(
+            long_segment.len(),
+            MAX_SEGMENT_LENGTH,
+            "Long segment length sanity check"
+        );
+        let input = format!("example.{long_segment}.test");
+
+        assert_eq!(validate_handle(&input), Ok(()));
+    }
+
+    #[test]
+    fn reject_max_length_segment() {
+        let long_segment = "loremipsumdolorsitametconsecteturadipiscingelitmaecenasegetjusto";
+        assert!(
+            long_segment.len() > MAX_SEGMENT_LENGTH,
+            "Long segment length sanity check"
+        );
+        let input = format!("example.{long_segment}.test");
+
+        assert_eq!(
+            validate_handle(&input),
+            Err(Error::InvalidSegment(long_segment.to_string()))
+        );
     }
 }
